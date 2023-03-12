6a74ba28561e51833ff46245406f1379b2deb02e
Qirun Dai <qirundai@sina.com>
2023-03-12 13:40:52 +0000

    add main script for wandb sweep

--- main_sweep.py
+++ main_sweep.py
@@ -4,8 +4,8 @@ import yaml
 
 if __name__ == "__main__":
 
-    with open('./sweep_mnist.yaml') as file:
+    with open('./sweep_cifar10.yaml') as file:
         sweep_cfg = yaml.load(file, Loader=yaml.FullLoader)
         print(sweep_cfg)
     sweep_id = wandb.sweep(sweep=sweep_cfg, project='Forward-Forward')
-    wandb.agent(sweep_id)
+    wandb.agent(sweep_id)   # Using grid search, so "count" is not needed.
